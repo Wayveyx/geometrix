9884ffe5e99ecd8107c18b603d6c3ee64fec8cba
Waves <creepykid1101@gmail.com>
2020-09-19 16:29:46 -0500

    why did i upload this

--- cmds/levels.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-const axios = require("axios");
-
-exports.run = async (bot, message, args, settings) => {
-    if (args[0] == undefined) return message.channel.send("Please provide a user ID.");
-    if (isNaN(args[0])) return message.channel.send("Please provide a valid user ID.");
-
-}
