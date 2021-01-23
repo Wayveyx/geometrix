e4a8ceb5b2096004f2cd836cba9d3023d9dc51c5
Wayveyx <creepykid1101@gmail.com>
2021-01-22 22:00:00 -0600

    added link command

--- cmds/help.js
+++ cmds/help.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 exports.run = async (bot, message, args) => {
     message.channel.send(new Discord.MessageEmbed()
         .setTitle("Help")
-        .setDescription("**g!levelinfo** - Get information on a level using it's ID.\n**g!userinfo** - See a user's stats by Username or ID.")
+        .setDescription("**g!levelinfo** - Get information on a level using it's ID.\n**g!userinfo** - See a user's stats by Username or ID.\n**g!link** - Link your in-game account to your discord account.")
         .setColor("#FFA500")
     );
 }
