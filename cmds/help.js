e7f8316aec1b90c34296ddf6002d8b69ddf8910c
Wayveyx <creepykid1101@gmail.com>
2021-01-23 19:33:47 -0600

    removed unnecessary space

--- cmds/help.js
+++ cmds/help.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 exports.run = async (bot, message, args) => {
     message.channel.send(new Discord.MessageEmbed()
         .setTitle("Help")
-        .setDescription("**g!levelinfo** - Get information on a level using it's ID.\n**g!userinfo** - See a user's stats by Username or ID.\n**g!link** - Link your in-game account to your discord account.\n **g!leaderboard (Stars, Demons, Coins, Usercoins)** - Leaderboard for linked users.")
+        .setDescription("**g!levelinfo** - Get information on a level using it's ID.\n**g!userinfo** - See a user's stats by Username or ID.\n**g!link** - Link your in-game account to your discord account.\n**g!leaderboard (Stars, Demons, Coins, Usercoins)** - Leaderboard for linked users.")
         .setColor("#FFA500")
     );
 }
