d0ee5e3aa64f2041e158034053187b11f81b2a2d
Wayveyx <creepykid1101@gmail.com>
2021-01-25 18:00:47 -0600

    pagination

--- cmds/leaderboard.js
+++ cmds/leaderboard.js
@@ -4,25 +4,59 @@ const base64 = require("base-64");
 
 exports.run = async (bot, message, args, settings) => {
     let type = args[0].toLowerCase();
-    let placement = 0;
     let types = ["stars", "demons", "coins", "usercoins"];
     if (!types.includes(type)) type = "stars";
-    axios.post(`${process.env.URL}/bot/lbs.php`, `type=${type}`)
-        .then(function (res) {
-            //console.log(res);
-            userList = res.data.split(":");
-            user = 0;
-            desc = "";
-            while (user < userList.length) {
-                placement++;
-                userInfo = userList[user].split("|");
-                desc += `**${placement}.** ${userInfo[1]} (<@${userInfo[0]}>) - **${userInfo[2]}**\n`;
-                user++;      
+    let rpages = [];
+    let pages;
+    let desc = "";
+    let getInfo = await axios.post(`${process.env.URL}/bot/lbs.php`, `type=${type}`)
+        .then(async function (res) {
+            //console.log(res.data);
+            let user = 0;
+            let placement = 0;
+            let page = 0;
+            pages = res.data.split("~");
+            while (page < pages.length) {
+                userList = pages[page].split(":");
+                while (user < userList.length) {
+                    placement++;
+                    userInfo = userList[user].split("|");
+                    desc += "**" + placement + ".** " + userInfo[1] + " (<@" + userInfo[0] + ">) - **" + userInfo[2] + "**\n";
+                    user++;
+                }
+                rpages.push(desc);
+                page++;
+                user = 0;
+                desc = "";
             }
-            lbtype = type.charAt(0).toUpperCase() + type.slice(1);
-            message.channel.send(new Discord.MessageEmbed()
-                .setTitle(`Leaderboard - ${lbtype}`)
-                .setColor("#01FF56")
-                .setDescription(desc));
         });
+    let cpage = 0;
+    let lbtype = type.charAt(0).toUpperCase() + type.slice(1);
+    let lbembed = new Discord.MessageEmbed()
+        .setTitle(`Leaderboard - ${lbtype}`)
+        .setColor("#01FF56")
+        .setDescription(rpages[cpage])
+        .setFooter(`Page ${cpage+1}/${pages.length}`);
+    message.channel.send(lbembed).then(msg => {
+        msg.react("803407762909429830");
+        msg.react("803408365115801670");
+        const backwardsFilter = (reaction, user) => reaction.emoji.id === "803407762909429830" && user.id === message.author.id;
+        const forwardsFilter = (reaction, user) => reaction.emoji.id === "803408365115801670" && user.id === message.author.id;
+        const backwards = msg.createReactionCollector(backwardsFilter, { time: 30000 });
+        const forwards = msg.createReactionCollector(forwardsFilter, { time: 30000 });
+        backwards.on('collect', r => {
+            if (cpage === 0) return;
+            cpage--;
+            lbembed.setDescription(rpages[cpage]);
+            lbembed.setFooter(`Page ${cpage+1}/${pages.length}`);
+            msg.edit(lbembed);
+        });
+        forwards.on('collect', r => {
+            if (cpage === pages.length-1) return;
+            cpage++;
+            lbembed.setDescription(rpages[cpage]);
+            lbembed.setFooter(`Page ${cpage+1}/${pages.length}`);
+            msg.edit(lbembed);
+        });
+    });
 }
