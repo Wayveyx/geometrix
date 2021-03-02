66ec6ef10daac8d9925db366eb9d158c5d9fd646
Wayveyx <creepykid1101@gmail.com>
2021-03-02 17:35:10 -0600

    automatic help command

--- cmds/help.js
+++ cmds/help.js
@@ -1,10 +1,38 @@
 const Discord = require("discord.js");
+const fs = require("fs");
 const axios = require("axios");
 
 exports.run = async (bot, message, args) => {
-    message.channel.send(new Discord.MessageEmbed()
-        .setTitle("Help")
-        .setDescription("**g!levelinfo** - Get information on a level using it's ID.\n**g!userinfo** - See a user's stats by Username or ID.\n**g!link** - Link your in-game account to your discord account.\n**g!leaderboard (Stars, Demons, Coins, Usercoins)** - Leaderboard for linked users.")
-        .setColor("#FFA500")
-    );
+    let desc = [];
+    if (args[0] != null && args[0].length > 0 && bot.commands.has(args[0])) {
+        let cmd = bot.commands.get(args[0]);
+        if (cmd.help.name == "help") return getCmds();
+        message.channel.send(new Discord.MessageEmbed()
+            .setTitle(`Help - ${cmd.help.name.charAt(0).toUpperCase() + cmd.help.name.slice(1)}`)
+            .setColor("#FFA500")
+            .setDescription(`${cmd.help.description}\n**Usage:** ${cmd.help.usage}`)
+            .addField(`Example(s)`, cmd.help.example)
+        );
+    } else getCmds();
+    async function getCmds() {
+        let getCmds = await fs.readdir('./cmds/', function (err, files) {
+            for (i = 0; i < files.length; i++) {
+                let file = files[i].split(".")[0];
+                let cmd = bot.commands.get(file);
+                if (cmd.help.name != "help") desc.push(`${cmd.help.usage} - ${cmd.help.description}`)
+            }
+            message.channel.send(new Discord.MessageEmbed()
+                .setTitle("Help")
+                .setDescription(desc.join("\n"))
+                .setColor("#FFA500")
+                .setFooter("Do g!help (command) to get help on a specific command."));
+        });
+    }
+}
+exports.help = {
+    name: "help",
+    description: "This command.",
+    usage: "**g!help**",
+    example: "get help",
+    aliases: []
 }
