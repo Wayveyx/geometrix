0ab63a6871b00763cde78ef7ef6208a2dfbea2c6
Wayveyx <creepykid1101@gmail.com>
2021-03-02 19:36:05 -0600

    b

--- cmds/levelinfo.js
+++ cmds/levelinfo.js
@@ -152,7 +152,8 @@ exports.run = async (bot, message, args, gjp, url) => {
             let decode = base64.decode(base);
             return decode;
         } catch (err) {
-            bot.channels.cache.get('776879580631269417').send(`${message.author} caused ${err.message} with ${args.join(" ")}`); //probably will only be triggered by voxalice   
+            bot.channels.cache.get('776879580631269417').send(`${message.author} caused ${err.message} with ${args.join(" ")}`); //probably will only be triggered by voxalice 
+            return base;
         }
     }
 }
